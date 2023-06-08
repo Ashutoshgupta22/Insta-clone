78c66682ec582adde07f687469efc9472f497cb8
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2023-06-09 01:24:03 +0530

    created basic profile layout

---
+++ app/src/main/java/com/aspark/instaclone/ui/ProfileActivity.kt
@@ -0,0 +1,19 @@
+package com.aspark.instaclone.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aspark.instaclone.R
+import com.aspark.instaclone.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
